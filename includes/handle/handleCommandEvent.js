d5e8097cab3d114676daae260d831d01f50479f8
rxabdullah0007 <rxabdullah0007@gmail.com>
2025-12-19 07:54:47 +0600

    Update handleCommandEvent.js

--- includes/handle/handleCommandEvent.js
+++ includes/handle/handleCommandEvent.js
@@ -1,37 +1,27 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
     const logger = require("../../utils/log.js");
-
     return function ({ event }) {
         const { allowInbox } = global.config;
         const { userBanned, threadBanned } = global.data;
         const { commands, eventRegistered } = global.client;
-
-        const { senderID, threadID } = event;
-        const fixUserIB = true;
-
-        if ((userBanned.has(String(senderID)) || threadBanned.has(String(threadID))) && fixUserIB) {
-            return;
-        }
-
+        var { senderID, threadID } = event;
+        senderID = String(senderID);
+        threadID = String(threadID);
+        if (userBanned.has(senderID) || threadBanned.has(threadID) || (allowInbox == false && senderID == threadID)) return;
         for (const eventReg of eventRegistered) {
             const cmd = commands.get(eventReg);
-            let getText2;
+            var getText2;
 
-            if (cmd.languages && typeof cmd.languages === 'object') {
+            if (cmd.languages && typeof cmd.languages == 'object') {
                 getText2 = (...values) => {
                     const commandModule = cmd.languages || {};
-
-                    if (!commandModule.hasOwnProperty(global.config.language)) {
-                        api.sendMessage(global.getText('handleCommand', 'notFoundLanguage', cmd.config.name), threadID);
-                        return;
-                    }
-
-                    let lang = cmd.languages[global.config.language][values[0]] || '';
-                    for (let i = values.length - 1; i >= 0; i--) {
-                        const expReg = RegExp('%' + i, 'g');
+                    if (!commandModule.hasOwnProperty(global.config.language)) 
+                        return api.sendMessage(global.getText('handleCommand','notFoundLanguage', cmd.config.name), threadID, messengeID); 
+                    var lang = cmd.languages[global.config.language][values[0]] || '';
+                    for (var i = values.length - 1; i >= 0; i--) {
+                        const expReg = RegExp('%' + (i + 1), 'g');
                         lang = lang.replace(expReg, values[i]);
                     }
-
                     return lang;
                 };
             } else {
@@ -39,19 +29,15 @@ module.exports = function ({ api, models, Users, Threads, Currencies }) {
             }
 
             try {
-                const Obj = {
-                    event,
-                    api,
-                    models,
-                    Users,
-                    Threads,
-                    Currencies,
-                    getText: getText2,
-                };
-
-                if (cmd) {
-                    cmd.handleEvent(Obj);
-                }
+                const Obj = {};
+                Obj.event = event;
+                Obj.api = api;
+                Obj.models = models;
+                Obj.Users = Users;
+                Obj.Threads = Threads;
+                Obj.Currencies = Currencies;
+                Obj.getText = getText2;
+                if (cmd) cmd.handleEvent(Obj);
             } catch (error) {
                 logger(global.getText('handleCommandEvent', 'moduleError', cmd.config.name), 'error');
             }
